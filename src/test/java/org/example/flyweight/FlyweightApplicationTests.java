8b91bf10508616acc7015b74714b53b1a7d7590a
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2024-07-07 16:08:06 -0300

    init

---
+++ src/test/java/org/example/flyweight/FlyweightApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.flyweight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyweightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
